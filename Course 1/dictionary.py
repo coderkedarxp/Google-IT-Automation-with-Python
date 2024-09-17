a565602ff86a0bde6481b97c76924fb56cf98170
coderkedarxp <132534804+coderkedarxp@users.noreply.github.com>
2024-09-17 19:04:19 +0530

    course 2 update

---
+++ Course 1/dictionary.py
@@ -0,0 +1,35 @@
+mydictionary={"banana":10,"apple":20,"mango":30,"pineapple":15}
+
+key = input(print("Enter the fruit: "))
+
+if key in mydictionary:
+    print("The number of {}s: {}".format(key,mydictionary[key]))
+else:
+    print("The {} is not found in the dictionary".format(key))
+ 
+# { 'a':[42], 'b':[23,6,1] }    Dictionary
+ 
+# {'^2', 'mc', ' equal', 'E'}    Set
+
+# [ 'a','b', 3, 4 ]      List
+
+# “call me ishmael”     String
+
+# ( 'commander','lambda')
+
+# Data structure        Tuple
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
